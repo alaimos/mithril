49409cd80e435bbfdc504b9e10e658d9c0582fc6
Salvatore Alaimo <alaimos@gmail.com>
2023-03-16 07:24:47 +0100

    Reader

---
+++ api/src/main/java/com/alaimos/MITHrIL/api/Data/Reader/RemoteVersionReader.java
@@ -0,0 +1,38 @@
+package com.alaimos.MITHrIL.api.Data.Reader;
+
+import com.alaimos.MITHrIL.api.Commons.Constants;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class RemoteVersionReader extends AbstractRemoteDataReader<String> {
+
+    public RemoteVersionReader() {
+        init();
+    }
+
+    private void init() {
+        setGzipped(false);
+        persisted(true).url(Constants.MITHRIL_VERSION_URL).file(Constants.MITHRIL_VERSION_FILE);
+    }
+
+    @Override
+    protected String realReader() {
+        //Ensures that all parameters are correctly set
+        init();
+        //Start Reading
+        String line;
+        try (BufferedReader r = new BufferedReader(new InputStreamReader(getInputStream()))) {
+            line = r.readLine();
+            if (line == null) {
+                throw new RuntimeException("Cannot gather current MITHrIL version.");
+            } else {
+                line = line.trim();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        return line;
+    }
+}
